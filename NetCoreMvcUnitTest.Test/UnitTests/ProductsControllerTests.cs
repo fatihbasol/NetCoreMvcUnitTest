5ed29d0de9c5ad800136579b8013f96536698c3a
Fatih Başol <fatih.basol@kariyer.net>
2022-01-11 15:03:05 +0300

    Added test class and first test completed

---
+++ NetCoreMvcUnitTest.Test/UnitTests/ProductsControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NetCoreMvcUnitTest.MVC.Controllers;
+using NetCoreMvcUnitTest.MVC.Data;
+using NetCoreMvcUnitTest.MVC.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NetCoreMvcUnitTest.Test.UnitTests
+{
+    public class ProductsControllerTests
+    {
+        private readonly Mock<IRepository<Product>> _repository;
+        private readonly ProductsController _controller;
+        private List<Product> products;
+
+        public ProductsControllerTests()
+        {
+            _repository = new Mock<IRepository<Product>>();
+            _controller = new ProductsController(_repository.Object);
+
+            products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Test", Price = 100, Color = "Red", Stock = 1050},
+                new Product() { Id = 2, Name = "Test 2", Price = 56, Color = "White", Stock = 450}
+            };
+        }
+
+        [Fact]
+        public async void Index_ActionExecutes_ReturnView()
+        {
+            var result = await _controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
